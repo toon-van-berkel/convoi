3a024d16e2f1a8bfb1b581eb6e3d287d4df570ee
Toon van Berkel <toonvanberkel203@gmail.com>
2026-06-23 19:27:10 +0200

    cleaning up release
    We don't have an release so this branch should be empty

--- target/x86_64-kernel/debug/deps/bootloader-ead89a9abccb8f83.d
+++
@@ -1,9 +0,0 @@
-/media/sf_convoi/target/x86_64-kernel/debug/deps/bootloader-ead89a9abccb8f83.d: /home/toon/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/bootloader-0.9.34/src/lib.rs /home/toon/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/bootloader-0.9.34/src/bootinfo/mod.rs /home/toon/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/bootloader-0.9.34/src/bootinfo/memory_map.rs
-
-/media/sf_convoi/target/x86_64-kernel/debug/deps/libbootloader-ead89a9abccb8f83.rlib: /home/toon/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/bootloader-0.9.34/src/lib.rs /home/toon/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/bootloader-0.9.34/src/bootinfo/mod.rs /home/toon/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/bootloader-0.9.34/src/bootinfo/memory_map.rs
-
-/media/sf_convoi/target/x86_64-kernel/debug/deps/libbootloader-ead89a9abccb8f83.rmeta: /home/toon/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/bootloader-0.9.34/src/lib.rs /home/toon/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/bootloader-0.9.34/src/bootinfo/mod.rs /home/toon/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/bootloader-0.9.34/src/bootinfo/memory_map.rs
-
-/home/toon/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/bootloader-0.9.34/src/lib.rs:
-/home/toon/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/bootloader-0.9.34/src/bootinfo/mod.rs:
-/home/toon/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/bootloader-0.9.34/src/bootinfo/memory_map.rs:
--- target/x86_64-kernel/debug/deps/convoi-7ede0363689caace.d
+++
@@ -1,5 +0,0 @@
-/media/sf_convoi/target/x86_64-kernel/debug/deps/convoi-7ede0363689caace.d: src/main.rs
-
-/media/sf_convoi/target/x86_64-kernel/debug/deps/convoi-7ede0363689caace: src/main.rs
-
-src/main.rs:
